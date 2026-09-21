7fb224729a5632deb7edeb82f265ad296f5e30a2
rathodvinod5 <rathodvinod5@gmail.com>
2026-09-22 01:28:26 +0530

    added configuration for javascript code check

---
+++ src/tools/financial-tools.js
@@ -0,0 +1,48 @@
+import { tool } from "@openai/agents";
+import { z } from "zod";
+
+export const getCompanyFinancials = tool({
+  name: "get_company_financials",
+  description: `Get key financial metrics for public company for a specific year`,
+  parameters: z.object({
+    company: z
+      .string()
+      .describe("Name of the company example NVIDIA, Apple etc"),
+    year: z
+      .number()
+      .int()
+      .min(2020)
+      .max(2026)
+      .describe("The financial year to retrieve"),
+  }),
+  strict: true,
+  execute: async ({ company, year }) => {
+    const financialData = {
+      NVIDIA: {
+        2025: {
+          revenue: 130.5,
+          revenueGrowth: 114,
+          grossMargin: 75.0,
+          operatingMargin: 61.6,
+        },
+      },
+
+      Apple: {
+        2025: {
+          revenue: 416.2,
+          revenueGrowth: 6,
+          grossMargin: 46.9,
+          operatingMargin: 31.0,
+        },
+      },
+    };
+
+    const companyData = financialData?.[company];
+
+    if (!companyData) {
+      return {
+        found: false,
+      };
+    }
+  },
+});
