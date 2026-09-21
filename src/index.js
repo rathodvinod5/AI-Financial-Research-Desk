0806b6b1e5df6ab35ec9d5100aa05e541b222f2d
rathodvinod5 <rathodvinod5@gmail.com>
2026-09-21 23:47:21 +0530

    Implemented first initial Financial research agent

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ import { z } from "zod";
 
 const researchAgent = new Agent({
   name: "Financial Research Agent",
-  instructions: ``,
+  instructions: `You are a financial research assistant
+  
+  Your job is to help users about publicly available financial and business information
+  
+  Do not provide personalised investment advice
+  Clearly distinguish facts from analysis.
+  Keep you answers consinse and structured.`,
 });
+
+async function main() {
+  const query = "Explain what revenue growth means when analyzing a company.";
+  const result = await run(researchAgent, query);
+
+  console.log("Final output: ", result?.finalOutput);
+}
+
+main().catch((err) => console.log("ERR: ", err));
